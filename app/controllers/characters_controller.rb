097b25354bcbabd41bcc42783f45b134fe755e7d
Cheryl Schaefer <CherylSchaefer@gmail.com>
2015-10-18 15:19:20 -0500

    added link to character form on main page, started character files - Amanda & Cheryl

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,11 @@
 class CharactersController < ApplicationController
+
+  def new
+    @character = Character.new
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :class, :race)
+  end
 end
