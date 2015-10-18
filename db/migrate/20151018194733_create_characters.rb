3033d82cbfd68190e7fadb8bb7d3816326e460af
Cheryl Schaefer <CherylSchaefer@gmail.com>
2015-10-18 15:03:38 -0500

    created character migration - Amanda & Cheryl

---
+++ db/migrate/20151018194733_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :class
+      t.string :race
+
+      t.timestamps
+    end
+  end
+end
