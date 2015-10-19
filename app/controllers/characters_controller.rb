0d6ed6ba1db140ae2f8b6d26150ce9a33eb2b2c4
Amanda Myers <elfgirl1981@yahoo.com>
2015-10-18 20:57:30 -0500

    saving character form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,8 +4,17 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
+  def create
+    @character = Character.new(character_params)
+	respond_to do |format|
+	  if @character.save
+	    format.html {redirect_to root_path, notice: "Character saved"}
+      end
+	end
+  end
+  
   private
   def character_params
-    params.require(:character).permit(:name, :class, :race)
+    params.require(:character).permit(:name, :class_type, :race)
   end
 end
